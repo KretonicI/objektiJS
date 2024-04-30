de0517ddd7cfc7ecb97b3627b381a2b7ad3a7cb3
Office11 <office11@ucione.local>
2024-04-30 18:35:33 +0200

    dodavanje 2

--- zadatak.js
+++ zadatak.js
@@ -19,8 +19,17 @@ var auto = {
     return this.brzina++;
     },
 
-    zakoci: function(vrijeme){
-    return vrijeme / 5;
+    zakoci: function(vrijeme) {
+    //return vrijeme / 5;
+    while (this.brzina >= 0) {
+        this.brzina = this.brzina - 20;
+        vrijeme--;
+        if (vrijeme ==0 || this.brzina == 0) {
+            break;
+        }
+    }
+    return this.brzina;
+
     },
     promijeniBoju: function (novaBoja){
         this.boja = novaBoja
@@ -32,3 +41,7 @@ console.log("auto JSON --> " + JSON.stringify(auto));
 console.log("Početna brzina: " + auto.brzina);
 console.log("Auto će  ubrzati na: " + auto.ubrzanje(2));
 console.log("Brzina nakon ubrzanja: " + auto.brzina);
+
+console.log(auto.zakoci(10));
+
+console.log("trenutna boja auta je: " + auto.boja);
